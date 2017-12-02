9e7b0e9821b450cda81f77daea92cd09d52a1500
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2017-12-02 13:13:47 -0800

    sema.cpp basic structure updated

--- sema.cpp
+++ sema.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <sys/ipc.h>
-#include <sys/sem.h>
+#include <sys/shm.h>
+#include <semaphore.h>
 #include "semaphore.h"
+
+const int MAXCHAR = 10;
+const int BUFFSIZE = 3;
+enum {GET_U, GET_V}; // set up names of my 2 semaphores
+
+int main() {
+    int shmid;
+	char *shmBUF;
+
+	SEMAPHORE sem(2);
+	sem.V(GET_U);
+	sem.V(GET_V);
+	shmid = shmget(IPC_PRIVATE, BUFFSIZE*sizeof(char), PERMS);
+	shmBUF = (char *)shmat(shmid, 0, SHM_RND);
+    
+    sem.remove();
+    sem.~SEMAPHORE();
+    return 0;
+}
